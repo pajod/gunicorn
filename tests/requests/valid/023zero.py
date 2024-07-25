pajod/gunicorn

commit 2dbe49de99f681923330d98f5b872730c73943d0

RFC compliant header field+chunk validation
* update HEADER_RE and HEADER_VALUE_RE to match the RFCs
* update chunk length parsing to disallow 0x prefix and digit-separating underscores.

gunicorn/http/body.py
@@ -86,10 +86,9 @@ class ChunkedReader(object):
         line, rest_chunk = data[:idx], data[idx + 2:]
 
         chunk_size = line.split(b";", 1)[0].strip()
-        try:
-            chunk_size = int(chunk_size, 16)
-        except ValueError:
+        if any(n not in b"0123456789abcdefABCDEF" for n in chunk_size):
             raise InvalidChunkSize(chunk_size)
+        chunk_size = int(chunk_size, 16)
 
         if chunk_size == 0:
             try:

gunicorn/http/message.py
@@ -21,7 +21,7 @@ MAX_REQUEST_LINE = 8190
 MAX_HEADERS = 32768
 DEFAULT_MAX_HEADERFIELD_SIZE = 8190
 
-HEADER_RE = re.compile(r"[\x00-\x1F\x7F()<>@,;:\[\]={} \t\\\"]")
+HEADER_RE = re.compile(r"[^!#$%&'*+\-.\^_`|~0-9a-zA-Z]")
 METH_RE = re.compile(r"[A-Z0-9$-_.]{3,20}")
 VERSION_RE = re.compile(r"HTTP/(\d+)\.(\d+)")
 

gunicorn/http/wsgi.py
@@ -18,7 +18,7 @@ from gunicorn import util
 # with sending files in blocks over 2GB.
 BLKSIZE = 0x3FFFFFFF
 
-HEADER_VALUE_RE = re.compile(r'[\x00-\x1F\x7F]')
+HEADER_VALUE_RE = re.compile(r'[^ \t\x21-\x7e\x80-\xff]')
 
 log = logging.getLogger(__name__)
 

commit e8ae3962c3556041511e47e60a99b1dd99aff8b3

chunk size parsing: raise InvalidChunkSize
ValueError is too generic, was InvalidChunkSize

Fixes: 2dbe49de99f681923330d98f5b872730c73943d0

gunicorn/http/body.py
@@ -91,6 +91,8 @@ class ChunkedReader(object):
             chunk_size = chunk_size.rstrip(b" \t")
         if any(n not in b"0123456789abcdefABCDEF" for n in chunk_size):
             raise InvalidChunkSize(chunk_size)
+        if len(chunk_size) == 0:
+            raise InvalidChunkSize(chunk_size)
         chunk_size = int(chunk_size, 16)
 
         if chunk_size == 0:

tests/requests/invalid/chunked_12.http
@@ -1,0 +1,11 @@
+POST /chunked_w_chunk_size_one HTTP/1.1\r\n
+Transfer-Encoding: chunked\r\n
+\r\n
+1\r\n
+\r\n
+POST /after1 HTTP/1.1\r\n
+Transfer-Encoding: identity\r\n
+\r\n
+POST /after2 HTTP/1.1\r\n
+Transfer-Encoding: identity\r\n
+\r\n

tests/requests/invalid/chunked_12.py
@@ -1,0 +1,2 @@
+from gunicorn.http.errors import ChunkMissingTerminator
+request = ChunkMissingTerminator

tests/requests/invalid/chunked_empty.http
@@ -1,0 +1,11 @@
+POST /chunked_w_empty_chunk_size HTTP/1.1\r\n
+Transfer-Encoding: chunked\r\n
+\r\n
+\r\n
+\r\n
+POST /after1 HTTP/1.1\r\n
+Transfer-Encoding: identity\r\n
+\r\n
+POST /after2 HTTP/1.1\r\n
+Transfer-Encoding: identity\r\n
+\r\n

tests/requests/invalid/chunked_empty.py
@@ -1,0 +1,2 @@
+from gunicorn.http.errors import InvalidChunkSize
+request = InvalidChunkSize

tests/requests/invalid/chunked_empty_ext.http
@@ -1,0 +1,11 @@
+POST /empty_but_extended_chunk_size HTTP/1.1\r\n
+Transfer-Encoding: chunked\r\n
+\r\n
+ ;foo=1\r\n
+\r\n
+POST /after1 HTTP/1.1\r\n
+Transfer-Encoding: identity\r\n
+\r\n
+POST /after2 HTTP/1.1\r\n
+Transfer-Encoding: identity\r\n
+\r\n

tests/requests/invalid/chunked_empty_ext.py
@@ -1,0 +1,2 @@
+from gunicorn.http.errors import InvalidChunkSize
+request = InvalidChunkSize

tests/requests/valid/023zero.http
@@ -1,0 +1,9 @@
+POST /one_chunks_mult_zero_size HTTP/1.1\r\n
+Transfer-Encoding: chunked\r\n
+\r\n
+000\r\n
+\r\n
+GET /second HTTP/1.1\r\n
+\r\n
+GET /third HTTP/1.1\r\n
+\r\n

tests/requests/valid/023zero.py
@@ -1,0 +1,27 @@
+req1 = {
+    "method": "POST",
+    "uri": uri("/one_chunks_mult_zero_size"),
+    "version": (1, 1),
+    "headers": [
+        ("TRANSFER-ENCODING", "chunked")
+    ],
+    "body": b"",
+}
+
+req2 = {
+    "method": "GET",
+    "uri": uri("/second"),
+    "version": (1, 1),
+    "headers": [],
+    "body": b""
+}
+
+req3 = {
+    "method": "GET",
+    "uri": uri("/third"),
+    "version": (1, 1),
+    "headers": [],
+    "body": b""
+}
+
+request = [req1, req2, req3]